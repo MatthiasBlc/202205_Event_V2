5aa69a8cfcf676bc70ff81427fd54f16c6ab6acc
MatthiasBlc <mat.bouloc@gmail.com>
2022-05-16 23:05:51 +0200

    fourth

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,9 +1,3 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: 'eventbrite.like@gmail.com'
 
-  def welcome_email(user)
-    @user = user
-    @url = 'https://immense-reaches-87063.herokuapp.com/'
-    mail(to: @user.email, subject: 'Welcome subject')
-  end
 end
