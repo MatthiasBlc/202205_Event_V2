e3e2dfa6271ee5df718ec12918df7a29f888c8ac
MatthiasBlc <mat.bouloc@gmail.com>
2022-05-16 23:01:19 +0200

    third

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,9 +2,11 @@ class User < ApplicationRecord
   has_many :events, through: :attendance, foreign_key: 'admin_id', class_name: 'Event'
   has_many :attendances, foreign_key: 'guest_id', class_name: 'Attendance'
 
-  after_create :welcome_send 
+  after_create :welcome_send
 
   def welcome_send
     UserMailer.welcome_email(self).deliver_now
   end
+
+
 end
