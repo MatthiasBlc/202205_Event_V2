cc4628b4d5635d35114dffec2a71420b60c59e87
MatthiasBlc <mat.bouloc@gmail.com>
2022-05-17 11:06:09 +0200

    d2 7

--- app/models/event.rb
+++ app/models/event.rb
@@ -4,7 +4,7 @@ class Event < ApplicationRecord
   has_many :guests, through: :attendance, class_name: 'User'
 
 
-  validates :start_date, presence: true
+  validates :start_date, presence: true, :start_is_future?
   validates :duration, presence: true, numericality: { greater_than_or_equal_to: 0 }
   validates :title, presence: true, length: { in: 5..140 }
   validates :description, presence: true, length: { in: 20..1000 }
