a896d96941fcc54b14b985ef8cd707afc05642f3
MatthiasBlc <mat.bouloc@gmail.com>
2022-05-17 10:23:30 +0200

    d2 first

---
+++ app/controllers/events_controller_controller.rb
@@ -0,0 +1,5 @@
+class EventsControllerController < ApplicationController
+  def index
+    
+  end
+end
--- config/environment.rb
+++ config/environment.rb
@@ -5,10 +5,10 @@ require_relative 'application'
 Rails.application.initialize!
 
 ActionMailer::Base.smtp_settings =   {
-  :address            => 'smtp.gmail.com',
+  :address            => 'in-v3.mailjet.com',
   :port               => 587,
   :domain             => 'google.com', #you can also use google.com
   :authentication     => :plain,
-  :user_name          => ENV['GMAIL_LOGIN'],
-  :password           => ENV['GMAIL_PWD']
+  :user_name          => ENV['MAILJET_API'],
+  :password           => ENV['MAILJET_KEY']
 }
